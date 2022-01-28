fc0ff1f0f41447482834bf39b64466ad95f322e5
EL HIMIR ABDERRAHIM <59322515+abdrrahimelh@users.noreply.github.com>
2022-01-28 11:45:43 +0000

    adding files

---
+++ Data collect/Machine1.py
@@ -0,0 +1,74 @@
+from pymongo import MongoClient, errors
+import numpy as np
+import pandas as pd
+import yfinance as yf
+from datetime import datetime
+import time
+from datetime import datetime
+import time
+from pytz import timezone
+tz = timezone('EST')
+start_time = time.time()
+
+# global variables for MongoDB host (default port is 27017)
+DOMAIN = '172.18.0.2'
+PORT = 27017
+try:
+ # try to instantiate a client instance
+    client = MongoClient(
+    host = [ str(DOMAIN) + ":" + str(PORT) ],
+    serverSelectionTimeoutMS = 3000, # 3 second timeout
+    username = "root",
+    password = "1234",
+    )
+
+    # print the version of MongoDB server if connection successful
+    print ("server version:", client.server_info()["version"])
+    mydb = client.stocks
+    mydb = client['stocktsla']
+    mycol = mydb["TSLA"]
+    print(mydb.list_collection_names())
+    # get the database_names from the MongoClient()
+    database_names = client.list_database_names()
+
+except errors.ServerSelectionTimeoutError as err:
+ # set the client and DB name list to 'None' and `[]` if exception
+    client = None
+    database_names = []
+    # catch pymongo.errors.ServerSelectionTimeoutError
+    print ("pymongo ERROR:", err)
+        
+print ("\ndatabases:", database_names)
+while (True):
+    now=datetime.now(tz) 
+    #tester si on est dans le market est ouvert (between 9:30 and 16:00 "est")
+    if (now.hour <=9) :
+        if (now.minute <30):
+            print("skipped")
+            time.sleep(10)
+            continue
+    if (now.hour >=16):
+        time.sleep(10)
+        continue
+    #si le market est fermé alors on execute pas le reste de la boucle
+    start_time = time.time()
+    #tester si la seconde est pair l'autre machine va tester si elle est impair
+    if (int(time.time())%2 ==0):
+        time.sleep(((100*int(time.time()))%100)/100)
+    if (int(time.time())%2 ==1):
+        try :
+            data = yf.download(tickers='TSLA', period='5m', interval='1m')
+            print(data.iloc[-1])
+            print("--- %s seconds ---" % (time.time() - start_time))
+        except: 
+            print("error")
+        mydata=data
+        mydata.reset_index(inplace=True)
+        data_dict = mydata.to_dict("records")
+        print(data_dict)
+        now = datetime.now()
+        for s in data_dict :
+            del s["Datetime"]
+        mycol.insert_one({"day":3,"hour":now.hour-5,"minute":now.minute,"second":now.second,"data":data_dict[1]})
+    print("--- %s seconds ---" % (time.time() - start_time))
+    time.sleep(abs(1-float((time.time() - start_time))))   
---
+++ Rest Server/Rest.py
@@ -0,0 +1,52 @@
+from flask import Flask, jsonify, request
+from pymongo import MongoClient, errors
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+# global variables for MongoDB host (default port is 27017)
+DOMAIN = '15.236.141.54'
+PORT = 27017
+ 
+client = MongoClient(
+        host = [ str(DOMAIN) + ":" + str(PORT) ],
+        serverSelectionTimeoutMS = 3000, # 3 second timeout
+        username = "root",
+        password = "1234",
+)
+    # print the version of MongoDB server if connection successful
+mydb = client.stocks
+mydb = client['stocktsla']
+mycol = mydb["TSLA"]
+col = mydb["TSLA30"]
+ 
+@app.route('/stock/TSLA', methods=['GET'])
+def get_all_frameworks():
+    output = []
+    for x in mycol.find():
+        output.append({'day':x['day'],'hour':x['hour']+1,'minute':x['minute'],'second' : x['second'], 'data' : x['data']})
+    return jsonify({'result' : output})
+ 
+@app.route('/stock/TSLA/30', methods=['GET'])
+def get_all_framew():
+    output = []
+    for x in col.find():
+        output.append({'Datetime':x['Datetime'],'Open' : x['Open'], 'High' : x['High'], 'Low' : x['Low'], 'Close' : x['Close']})
+    return jsonify(output)
+ 
+@app.route('/stock/TSLA/<day>/<hour>/<minute>/<second>', methods=['GET'])
+def get_all_framework(day,hour,minute,second):
+    output = []
+    q = mycol.find_one({'day':int(day),'hour':int(hour),'minute':int(minute),'second':int(second)})
+ 
+    if q:
+        output = {'day':q['day'],'hour':q['hour'],'minute':q['minute'],'second' : q['second'], 'data' : q['data']}
+    else:
+        output = 'No results found'
+ 
+    return jsonify({'result' : output})
+ 
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
+ 
+ 
